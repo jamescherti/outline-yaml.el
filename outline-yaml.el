718720db64fab8a1193982ed7c335beb169f4227
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-14 10:39:41 -0500

    Update copyright year

--- outline-yaml.el
+++ outline-yaml.el
@@ -1,6 +1,6 @@
 ;;; outline-yaml.el --- Code folding and Outline for Yaml files  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025  James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2026  James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 1.0.2
